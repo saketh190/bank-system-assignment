49d34ae098003a3362eba359998a556edec25407
saketh <140319029+saketh190@users.noreply.github.com>
2025-07-26 13:04:38 +0530

    added default port

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
+app.get('/', (req, res) => {
+  res.send('Banking System API is running');
+});
 app.use('/api/loan', loanRoutes);
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
